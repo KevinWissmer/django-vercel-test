f235b73d63b592204a65ae5d82f242415b10a1e4
Kevin Wißmer <74767313+KevinWissmer@users.noreply.github.com>
2024-09-26 12:33:22 +0200

    static files added

--- api/settings.py
+++ api/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for api project.
 
@@ -131,6 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
